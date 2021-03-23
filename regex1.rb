097ca89a71452263fd0998429303dda7b9736c30
josh richards <joshuarichards001@gmail.com>
2021-03-23 13:25:44 +1300

    account for repeat alternations

--- regex1.rb
+++ regex1.rb
@@ -1,7 +1,7 @@
 # Inspiration from this blog post https://nickdrane.com/build-your-own-regex/
 # expression_file = "tests/#{ARGV[0]}"
 # target_file = "tests/#{ARGV[1]}"
-file_name = "parens"
+file_name = "all"
 expression_file = "tests/#{file_name}-expressions.txt"
 target_file = "tests/#{file_name}-targets.txt"
 expected_file = target_file[0, target_file.length - 12] + "-expected.txt"
@@ -27,17 +27,22 @@ def run(expression_array, target_array, expected_array)
   correct_count = 0
   wrong_count = 0
   expression_array.zip(target_array, expected_array).each do |expression, target, expected|
-    if match(expression.chars, target.chars)
-      result = "YES: #{expression} with #{target}"
-    else
-      result = "NO:  #{expression} with #{target}"
+    begin
+      target.match(expression)
+      if match(expression.chars, target.chars)
+        result = "YES: #{expression} with #{target}"
+      else
+        result = "NO:  #{expression} with #{target}"
+      end
+    rescue RegexpError => e
+      result = "SYNTAX ERROR: #{expression} with #{target}"
     end
     if result == expected
       correct_count += 1
       puts "CORRECT => " + result
     else
       wrong_count += 1
-      puts ">>>> WRONG => " + result
+      puts "WRONG => " + result
     end
   end
   puts correct_count.to_f/(correct_count.to_f+wrong_count.to_f)
@@ -84,9 +89,29 @@ end
 def parentheses_match (expression, target)
   closed_parentheses_index = find_closing_paren(expression)
   group = expression[1, closed_parentheses_index-1]
+  if group.last == '*'
+    return false
+  end
   if expression[closed_parentheses_index + 1] == '*'
     remainder = expression.drop(closed_parentheses_index + 2)
-    (match(group, target[0, group.length]) && match(expression, target.drop(group.length))) || match(remainder, target)
+    if group.include?('|') && find_outer_pipe(group) != 99
+      pipe_index = find_outer_pipe(group)
+      before = group[0, pipe_index]
+      after = group[pipe_index+1, group.length]
+      (match(before, target[0, before.length]) && match(expression, target.drop(before.length))) || match(remainder, target) ||
+      (match(after, target[0, after.length]) && match(expression, target.drop(after.length))) || match(remainder, target)
+    else
+      (match(group, target[0, group.length]) && match(expression, target.drop(group.length))) || match(remainder, target)
+    end
+  elsif group.include?('|') && find_outer_pipe(group) != 99
+    remainder = expression.drop(closed_parentheses_index + 1)
+    pipe_index = find_outer_pipe(group)
+    before = group[0, pipe_index]
+    after = group[pipe_index+1, group.length]
+    lits_before = before - ["("] - [")"]
+    lits_after = after - ["("] - [")"]
+    match(before, target[0, lits_before.length]) && match(remainder, target.drop(lits_before.length)) ||
+    match(after, target[0, lits_after.length]) && match(remainder, target.drop(lits_after.length))
   else
     remainder = expression.drop(closed_parentheses_index + 1)
     lits = group - ["("] - [")"]
@@ -104,9 +129,30 @@ def find_closing_paren(array)
       counter += 1
     elsif char == ')'
       counter -= 1
+    elsif close_pos == 99
+      break
     end
   end
   close_pos
+end
+
+def find_outer_pipe(array)
+  pipe_pos = -1
+  counter = 1
+  while counter > 0
+    pipe_pos += 1
+    char = array[pipe_pos]
+    if char == '('
+      counter += 1
+    elsif char == ')'
+      counter -= 1
+    elsif char == '|' && counter == 1
+      return pipe_pos
+    elsif pipe_pos == 99
+      return pipe_pos
+      break
+    end
+  end
 end
 
 run(expression_array, target_array, expected_array)
